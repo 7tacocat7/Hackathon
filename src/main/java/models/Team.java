cc6f94e810678fbaf06f09a03a32ca9906617b01
Carson Canales <cc020889@icloud.com>
2017-08-11 09:25:41 -0700

    add Team attributes- teamName,description,members

--- src/main/java/models/Team.java
+++ src/main/java/models/Team.java
@@ -1,4 +1,10 @@
 package models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Team {
+    private String teamName;
+    private String description;
+    private List<String> members = new ArrayList<String>();
 }
