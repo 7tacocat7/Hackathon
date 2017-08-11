7e52c6b1fd694aa9951115005b138401924f7742
Carson Canales <cc020889@icloud.com>
2017-08-11 10:46:08 -0700

    pass second test newTeam_instantiatesCorrectly()

--- src/main/java/models/Team.java
+++ src/main/java/models/Team.java
@@ -6,5 +6,26 @@ import java.util.List;
 public class Team {
     private String teamName;
     private String description;
-    private List<String> members = new ArrayList<String>();
+    private String member;
+    private  List<String> members = new ArrayList<String>();
+    public static List<Team>instances = new ArrayList<Team>();
+
+    public Team(String teamName, String description, String member){
+        this.teamName = teamName;
+        this.description = description;
+        this.member = member;
+        members.add(member);
+        instances.add(this);
+
+    }
+    public List<String> getMembers() {
+        return members;
+    }
+
+
+    public static List<Team> getAllTeams() {
+        return instances;
+    }
+
 }
+
---
+++ src/test/java/models/TeamTest.java
@@ -0,0 +1,29 @@
+package models;
+
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class TeamTest {
+    @Test
+    public void newTeam_instantiatesCorrectly() throws Exception {
+        Team testTeam = new Team ("1","the best","Sue");
+        assertEquals(true, testTeam instanceof Team);
+    }
+    @Test
+    public void all_returnsAllInstancesOfTeam_true() throws Exception{
+        Team firstTeam = new Team("tigers","we are the best","john");
+        Team secondTeam = new Team("lions","the lions team is focused on java","Phill");
+        assertTrue(Team.getAllTeams().contains(firstTeam));
+        assertTrue(Team.getAllTeams().contains(secondTeam));
+    }
+    @Test
+    public void all_returnsAllInstancesOfMembers_true() throws Exception {
+        Team testTeam = new Team ("bears","the best team on the planet","collin");
+        assertTrue(testTeam.getMembers().contains("bob"));
+    }
+
+
+}
