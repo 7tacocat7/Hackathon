00da433d3b56640d737db00ff6dd89f162f6b0a5
Carson Canales <cc020889@icloud.com>
2017-08-11 11:37:49 -0700

    pass third test  all_returnsAllInstancesOfMembers_true added getter to test

--- src/main/java/models/Team.java
+++ src/main/java/models/Team.java
@@ -6,27 +6,23 @@ import java.util.List;
 public class Team {
     private String teamName;
     private String description;
-    private String member;
+    //    private String member;
     private  List<String> members = new ArrayList<String>();
     public static List<Team>instances = new ArrayList<Team>();
 
-    public Team(String teamName, String description, String member){
+    public Team(String teamName, String description){
         this.teamName = teamName;
         this.description = description;
-        this.member = member;
-        this.members.add(member);
+//        this.member = member;
+        members = members;
         instances.add(this);
 
     }
+//    getters
     public List<String> getMembers() {
         return members;
     }
-    public void setMembers(List<String> members) {
-        this.members = members;
-    }
-    public void setMember(String member) {
-        this.member = member;
-    }
+
 
 
 
--- src/test/java/models/TeamTest.java
+++ src/test/java/models/TeamTest.java
@@ -9,20 +9,22 @@ import static org.junit.Assert.*;
 public class TeamTest {
     @Test
     public void newTeam_instantiatesCorrectly() throws Exception {
-        Team testTeam = new Team ("1","the best","Sue");
+        Team testTeam = new Team ("1","the best");
         assertEquals(true, testTeam instanceof Team);
     }
     @Test
     public void all_returnsAllInstancesOfTeam_true() throws Exception{
-        Team firstTeam = new Team("tigers","we are the best","john");
-        Team secondTeam = new Team("lions","the lions team is focused on java","Phill");
+        Team firstTeam = new Team("tigers","we are the best");
+        Team secondTeam = new Team("lions","the lions team is focused on java");
         assertTrue(Team.getAllTeams().contains(firstTeam));
         assertTrue(Team.getAllTeams().contains(secondTeam));
     }
     @Test
     public void all_returnsAllInstancesOfMembers_true() throws Exception {
-        Team testTeam = new Team ("bears","the best team on the planet","collin");
-        Team testTeam2 = new Team ("cubs","the best","ryan");
+        Team testTeam = new Team ("bears","the best team on the planet");
+        Team testTeam2 = new Team ("cubs","the best");
+        testTeam.getMembers().add("collin");
+        testTeam2.getMembers().add("ryan");
         assertTrue(testTeam.getMembers().contains("collin"));
         assertTrue(testTeam2.getMembers().contains("ryan"));
     }
