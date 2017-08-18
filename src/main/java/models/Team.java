765b4cd1ed2521aad65968967bfdfbe289cbea8d
Carson Canales <cc020889@icloud.com>
2017-08-18 16:49:20 -0700

    delete team test file

--- src/main/java/models/Team.java
+++ src/main/java/models/Team.java
@@ -5,31 +5,25 @@ import java.util.List;
 
 public class Team {
     private String teamName;
-
-
     private String description;
-
-    //    private String name;
-    private  List<String> members = new ArrayList<String>();
-    public static List<Team>instances = new ArrayList<Team>();
-
-
+//    private  List<String> members = new ArrayList<String>();
+//    public static List<Team>instances = new ArrayList<Team>();
     private int teamId;
 //    private String teamMember;
 
     public Team(String teamName, String description,String name){
         this.teamName = teamName;
         this.description = description;
-        this.members.add(name);
-        instances.add(this);
-        this.teamId = instances.size();
+//        this.members.add(name);
+//        instances.add(this);
+//        this.teamId = instances.size();
 
     }
 //    getters
 
-    public List<String> getMembers() {
-        return members;
-    }
+//    public List<String> getMembers() {
+//        return members;
+//    }
 
     public String getTeamName() {
         return teamName;
@@ -37,33 +31,33 @@ public class Team {
     public String getDescription() {
         return description;
     }
-    public void getAddMember(String name){
-        this.members.add(name);
-    }
+//    public void getAddMember(String name){
+//        this.members.add(name);
+//    }
 
 
 
 
-    public static List<Team> getAllTeams() {
-        return instances;
-    }
+//    public static List<Team> getAllTeams() {
+//        return instances;
+//    }
 
-    public static void clearAllTeams(){
-        instances.clear();
-    }
+//    public static void clearAllTeams(){
+//        instances.clear();
+//    }
 
     public  int getTeamId() {
         return teamId;
     }
 
-    public static Team findById(int teamId) {
-        return instances.get(teamId - 1);
-    }
-
-
-    public void setMembers(String name) {
-        this.members.add(name);
-    }
+//    public static Team findById(int teamId) {
+//        return instances.get(teamId - 1);
+//    }
+//
+//
+//    public void setMembers(String name) {
+//        this.members.add(name);
+//    }
 
     public void updateTeamName(String teamName) {
         this.teamName = teamName;
