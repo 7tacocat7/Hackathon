94db4f47dcb04fb4084011c3308bab591b231c52
Carson Canales <cc020889@icloud.com>
2017-08-18 15:37:58 -0700

    fail test for addTeamMemberAddsNewNameToList

--- src/test/java/models/TeamTest.java
+++ src/test/java/models/TeamTest.java
@@ -81,6 +81,9 @@ public class TeamTest {
     }
     @Test
     public void addTeamMemberAddsNewNameToList() throws Exception{
+        Team team = setupNewTeam();
+        team.setMembers("john");
+        assertTrue(team.getMembers().contains("john"));
 
     }
 
