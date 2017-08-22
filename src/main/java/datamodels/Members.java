0e6d812c32fbcb359b391638f16de1cff6030b90
Carson Canales <cc020889@icloud.com>
2017-08-21 19:29:40 -0700

    make Member interface, memberdoa, and  memberdoatest files

--- src/main/java/datamodels/Members.java
+++ src/main/java/datamodels/Members.java
@@ -1,4 +1,42 @@
 package datamodels;
 
 public class Members {
+    private String memberName;
+    private String memberId;
+
+//getters
+    public String getMemberName() {
+        return memberName;
+    }
+
+    public String getMemberId() {
+        return memberId;
+    }
+//setters
+
+    public void setMemberName(String memberName) {
+        this.memberName = memberName;
+    }
+
+    public void setMemberId(String memberId) {
+        this.memberId = memberId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Members members = (Members) o;
+
+        if (!memberName.equals(members.memberName)) return false;
+        return memberId != null ? memberId.equals(members.memberId) : members.memberId == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = memberName.hashCode();
+        result = 31 * result + (memberId != null ? memberId.hashCode() : 0);
+        return result;
+    }
 }
